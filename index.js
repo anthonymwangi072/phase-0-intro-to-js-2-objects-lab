ba70feb8c95fdf46ab1b8bd03c6cbef8e355743c
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-09 10:01:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee = {
+  name: "Anthony Mwaniki",
+  streetAddress: "Mwangi iriika crescent"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  console.log(newEmployee);
+  newEmployee[key] = value;
+  return newEmployee;
+
+
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee, "streetAddress", "Mwangi iriika crescent"));
+
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key,value) {
+   employee[key] = value;
+  return employee;
+}
+
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "mwangi iriika crescent"));
+
+function deleteFromEmployeeByKey(employee, key) {
+const newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+
+}
+console.log(deleteFromEmployeeByKey(employee, "streetAddress", "mwangi iriika crescent"));
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+
+}
